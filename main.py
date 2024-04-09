e8a612fda7620a2045097aae5d35f01c98ef69e6
Harding, Matt <matt.harding@refinitiv.com>
2024-04-09 13:31:02 +0100

    Model performance report notes and code refactor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import torch
 from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.optim as optim
 from torch import save, load
@@ -12,27 +11,19 @@ from Utils import WhaleDataset
 from Classifiers import WhaleClassifier 
 
 if __name__ == "__main__":
-    '''
-        Step 1: Converts PIL image to a PyTorch tensor, scaling ther pixel values to the range [0,1]
-
-        MATT to CONSIDER!
-        Step 2: Normalizes the tensor by subtracting the mean and dividing by th e standard deviation. The provided mean and standard deviation values are typical for ImageNet data,
-    '''
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) 
-    ])
-
-
+    # Load data
     dataset = WhaleDataset(csv_file='Datasets/train.csv', image_dir='Datasets/train_images', transform=transform)
-    train_data, test_data = train_test_split(dataset, test_size=0.95, random_state=42)
+
+    # Split data in train and test
+    train_data, test_data = train_test_split(dataset, test_size=0.2, random_state=42)
     train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
     test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
 
-    # For this assessment we only considfer inviduals within the training dataset
-    # This line would need to be modified to consider out of 
+    # For this assessment we only consider inviduals within the training dataset
+    # This line would need to be modified to consider out of distribution
     num_classes = dataset.data['individual_id'].nunique()
 
+    # Instantiate Image Classifier
     model = WhaleClassifier(num_classes)
 
     # Why are we using Cross Entropy?
